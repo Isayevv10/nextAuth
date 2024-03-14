e76d1ea8b94f8b66fa81ddb23fcec935bab4dade
Allahverdi <isaevallahverdi@gmail.com>
2024-03-15 00:32:49 +0400

    fixes/redirect

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -2,22 +2,30 @@
 
 import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 const page = () => {
   const { data: session, status } = useSession();
   console.log(status);
 
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      redirect("/login");
+    }
+  }, [status]);
+
   return (
     <div>
-      <p> HERE IS DASHBOARD PAGE</p>
-      <p>Name: {session?.user?.name}</p>
-      <p>Email: {session?.user?.email}</p>
-      <button
-        onClick={() => signOut({ callbackUrl: "/login", redirect: true })}
-      >
-        Log out
-      </button>
+      <>
+        <p> HERE IS DASHBOARD PAGE</p>
+        <p>Name: {session?.user?.name}</p>
+        <p>Email: {session?.user?.email}</p>
+        <button
+          onClick={() => signOut({ callbackUrl: "/login", redirect: true })}
+        >
+          Log out
+        </button>
+      </>
     </div>
   );
 };
